c12108b30ad8d2fb61069dfa9bcb009ca0120236
Blayze <blayzestone@protonmail.com>
2020-05-20 13:24:46 -0700

    <UpdateMovieForm> return jsx form

--- client/src/components/UpdateMovieForm.js
+++ client/src/components/UpdateMovieForm.js
@@ -1,7 +1,25 @@
 import React from "react";
 
 const UpdateMovieForm = (props) => {
-  return <div>Update Movie Route</div>;
+  return (
+    <form
+      style={{
+        width: "60%",
+        margin: "0 auto",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <label htmlFor="title">Title:</label>
+      <input type="text" id="title" name="title" />
+      <label htmlFor="director">Director:</label>
+      <input type="text" id="director" name="director" />
+      <label htmlFor="metascore">Metascore:</label>
+      <input type="text" id="metascore" name="metascore" />
+      <label htmlFor="stars">Stars:</label>
+      <input type="text" id="stars" name="stars" />
+    </form>
+  );
 };
 
 export default UpdateMovieForm;
